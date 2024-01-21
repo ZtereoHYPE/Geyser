0a1e155c312132e3d0b0a56e2e5982f383f1cfc4
Greg Mazonakis <grigoriosmaz@gmail.com>
2024-01-21 21:21:49 +0100

    Added checkstyle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     `eclipse`
     id("geyser.build-logic")
     id("io.freefair.lombok") version "6.3.0" apply false
+    id("checkstyle")
 }
 
 allprojects {
@@ -31,10 +32,20 @@ subprojects {
         plugin("java-library")
         plugin("io.freefair.lombok")
         plugin("geyser.build-logic")
+        plugin("checkstyle")
     }
 
     when (this) {
         in platforms -> plugins.apply("geyser.platform-conventions")
         else -> plugins.apply("geyser.base-conventions")
     }
+
+    tasks.withType<Checkstyle> {
+        configFile = rootProject.file("config/checkstyle/checkstyle.xml")
+    }
+
+    tasks.named("build") {
+        dependsOn("checkstyleMain", "checkstyleTest")
+    }
+
 }
